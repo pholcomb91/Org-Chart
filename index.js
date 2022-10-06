cb489994a043537a82b2892eabfd389fb59eb7cc
Peter Holcomb <pholcomb91@gmail.com>
2022-10-06 15:56:33 -0700

    feat: HTML page is created

--- index.js
+++ index.js
@@ -5,20 +5,83 @@ const Engineer = require('./library/engineer')
 const Intern = require('./library/intern')
 
 const employeeArray = [];
-const generateHTML = ({ name, location, github, linkedin }) =>
-    ``;
 
 const finish = () => {
-//loop through array
-return `
 
-${employeeArray.map(employee => {
-    //use employee.getRole() to identify role
-    //if Manager - return Manager html block
-    //else if Engineer - return Engineer html block
-    //else if Intern - return Intern html block
-})}
-`
+const generateHTML = (employeeArray) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <title>My Team</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-primary shadow m-5 rounded">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1 fs-1">My Team</span>
+        </div>
+    </nav>
+    <main class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 justify-content-center">
+    ${employeeArray.map(employee => {
+        const role = employee.getRole()
+        if (role === 'Manager') {
+            const html =
+            `<div class="col card shadow p-3 m-5 bg-body rounded" style="width: 18rem;">
+                <div class="card-body text-center">
+                    <h4>${employee.name}</h4>
+                <h5 class="card-title">${role}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${employee.id}</li>
+                    <a href="mailto:${employee.email}" class="list-group-item">Email: ${employee.email}</a>
+                    <li class="list-group-item">Office #${employee.office}</li>
+                </ul>
+                </div>
+            </div>`
+            return html;
+        } else if (role === 'Engineer') {
+            const html =
+            `<div class="col card shadow p-3 m-5 bg-body rounded" style="width: 18rem;">
+                <div class="card-body text-center">
+                    <h4>${employee.name}</h4>
+                <h5 class="card-title">${role}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${employee.id}</li>
+                    <a href="mailto:${employee.email}" class="list-group-item">Email: ${employee.email}</a>
+                    <li href="https://github.com/${employee.github}" class= "list-group-item">Github: ${employee.github}</li>
+                </ul>
+                </div>
+            </div>`
+            return html;
+        } else if (role === 'Intern') {
+            const html =
+            `<div class="col card shadow p-3 m-5 bg-body rounded" style="width: 18rem;">
+                <div class="card-body text-center">
+                    <h4>${employee.name}</h4>
+                <h5 class="card-title">${role}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${employee.id}</li>
+                    <a href="mailto:${employee.email}" class="list-group-item">Email: ${employee.email}</a>
+                    <li class="list-group-item">School: ${employee.school}</li>
+                </ul>
+                </div>
+            </div>`
+            return html;
+        } else {
+            const html =
+            `No employees were returned`
+            return html;
+        }
+    })}
+    </main>
+</body>
+</html>`
+    const htmlPageContent = generateHTML(employeeArray);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
 }
 const promptEngineer = () => {
     inquirer
@@ -26,33 +89,62 @@ const promptEngineer = () => {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is your name?',
+                message: 'What is their name?',
             },
             {
                 type: 'input',
                 name: 'id',
-                message: 'Where are you from?',
+                message: 'What is their ID?',
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'What is your favorite hobby?',
+                message: 'What is their email?',
             },
             {
                 type: 'input',
                 name: 'github',
-                message: 'What is your favorite food?',
+                message: 'What is their Github username?',
             },
 
         ])
         .then(({ name, id, email, github }) => {
-            //const htmlPageContent = generateHTML(answers);
-
-            // fs.writeFile('index.html', htmlPageContent, (err) =>
-            //   err ? console.log(err) : console.log('Successfully created index.html!')
-            // );
+            
             const engineer = new Engineer(name, id, email, github)
             employeeArray.push(engineer)
+            console.log(employeeArray)
+            promptOther();
+        });
+}
+const promptIntern = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is their name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is their ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is their email?',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school are they attending?',
+            },
+
+        ])
+        .then(({ name, id, email, school }) => {
+            
+            const intern = new Intern(name, id, email, school)
+            employeeArray.push(intern)
             console.log(employeeArray)
             promptOther();
         });
@@ -62,7 +154,7 @@ const promptOther = () => {
         {
             type: 'list',
             name: 'name',
-            message: 'Who do you want to add?',
+            message: 'Who do you want to add to your team?',
             choices: ['Engineer', 'Intern', 'Finish']
         },
     ])
@@ -93,26 +185,22 @@ const promptManager = () => {
             {
                 type: 'input',
                 name: 'id',
-                message: 'Where are you from?',
+                message: 'What is your ID?',
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'What is your favorite hobby?',
+                message: 'What is your email?',
             },
             {
                 type: 'input',
                 name: 'officeNumber',
-                message: 'What is your favorite food?',
+                message: 'What is your office number?',
             },
 
         ])
         .then(({ name, id, email, officeNumber }) => {
-            //const htmlPageContent = generateHTML(answers);
-
-            // fs.writeFile('index.html', htmlPageContent, (err) =>
-            //   err ? console.log(err) : console.log('Successfully created index.html!')
-            // );
+            
             const manager = new Manager(name, id, email, officeNumber)
             employeeArray.push(manager)
             console.log(employeeArray)
